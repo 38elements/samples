73f8c193fc9d7387defd8957a20bc2d17c3b705a
38elements <mh19820223@gmail.com>
2016-09-24 21:55:20 +0900

    test

--- sample-mocha/tests/test_1.js
+++ sample-mocha/tests/test_1.js
@@ -1,10 +1,23 @@
 const co = require('co');
+const Nightmare = require('nightmare');
+let url = 'http://example.com';
+let title = 'example title';
 
-describe('foo', function() {
-    it('1 + 2 = 3', function() {
+describe('Web page', function() {
+    it('title', function() {
+        this.timeout(10000);
+        let nightmare = Nightmare({
+            show: false,
+            waitTimeout: 5000,
+            gotoTimeout: 5000,
+            partition: 'part_' + Math.random()
+        });
         return co(function * () {
-            let value = yield Promise.resolve(1 + 2);
-            (1 + 2).should.equal(3);
+            let title = yield nightmare.goto(url).wait('#wrapper').title();
+            title.should.equal(title);
+        }).catch((e) => {
+            nightmare.end();
+            throw e;
         })
     });
     it('1 + 2 = 3', function() {
