cf8f856200aa986c566c4bdf42a2a5a118e4700a
38elements <mh19820223@gmail.com>
2016-09-24 14:01:16 +0900

    update

---
+++ sample-mocha/tests/test_2.js
@@ -0,0 +1,16 @@
+const co = require('co');
+
+describe('bar', function() {
+    it('1 + 2 = 3', function() {
+        return co(function * () {
+            let value = yield Promise.resolve(1 + 2);
+            (1 + 2).should.equal(3);
+        })
+    });
+    it('1 + 2 = 3', function() {
+        return co(function * () {
+            let value = yield Promise.resolve(1 + 2);
+            (1 + 2).should.equal(3);
+        })
+    });
+});
