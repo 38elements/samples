f8303c1ab955ca6d6aea1f0380966ca2d2818072
38elements <mh19820223@gmail.com>
2016-09-26 23:32:15 +0900

    update

--- sample-mocha/test_util.js
+++ sample-mocha/test_util.js
@@ -2,11 +2,11 @@ const chai = require('chai');
 const Nightmare = require('nightmare');
 chai.should();
 global.assert = chai.assert
-global.getNightmare = function() {
+global.getNightmare = function(show=false, waitTimeout=5000, gotoTimeout=5000) {
     return Nightmare({
-        show: false,
-        waitTimeout: 5000,
-        gotoTimeout: 5000,
+        show,
+        waitTimeout,
+        gotoTimeout,
         webPreferences: {
             partition: 'part_' + Math.random()
         }
