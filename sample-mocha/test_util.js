805fa4b53dddadc4db6a5a724b66a6cb95170060
38elements <mh19820223@gmail.com>
2016-09-26 21:47:05 +0900

    update

--- sample-mocha/test_util.js
+++ sample-mocha/test_util.js
@@ -1,2 +1,14 @@
-let chai = require('chai');
+const chai = require('chai');
+const Nightmare = require('nightmare');
 chai.should();
+global.assert = chai.assert
+global.getNightmare = function() {
+    return Nightmare({
+        show: false,
+        waitTimeout: 5000,
+        gotoTimeout: 5000,
+        webPreferences: {
+            partition: 'part_' + Math.random()
+        }
+    });
+};
--- sample-mocha/tests/test_1.js
+++ sample-mocha/tests/test_1.js
@@ -1,23 +1,22 @@
 const co = require('co');
-const Nightmare = require('nightmare');
 let url = 'http://example.com';
-let title = 'example title';
+let title = 'Title';
 
 describe('Web page', function() {
+    let nightmare;
+    beforeEach(function () {
+        nightmare = getNightmare() 
+    });
+    afterEach(function () {
+        nightmare.end();
+    });
     it('title', function() {
         this.timeout(10000);
-        let nightmare = Nightmare({
-            show: false,
-            waitTimeout: 5000,
-            gotoTimeout: 5000,
-            partition: 'part_' + Math.random()
-        });
         return co(function * () {
-            let title = yield nightmare.goto(url).wait('#wrapper').title();
+            let title = yield nightmare.goto(url).wait('body').title();
             title.should.equal(title);
         }).catch((e) => {
-            nightmare.end();
-            throw e;
+            assert.fail(0, 1, e);
         })
     });
     it('1 + 2 = 3', function() {
