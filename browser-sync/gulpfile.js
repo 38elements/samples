70e3c9637431a052d0a87d254588455b0b16a2bb
38elements <mh19820223@gmail.com>
2017-01-14 15:32:42 +0900

    Update gulpfile.js

--- browser-sync/gulpfile.js
+++ browser-sync/gulpfile.js
@@ -7,9 +7,11 @@ gulp.task("start", function() {
     bs.init({
         proxy: "http://localhost:4000",
         serveStatic: [{
+            // http://localhost:4000/publicをpublicディレクトリにマッピング
             route: '/public',
             dir: 'public'
         }],
+        // 最初にhttp://localhost:3000/publicにアクセス
         startPath: "/public",
         port: 3000
     });
